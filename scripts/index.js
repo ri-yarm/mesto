a9e44e4316df5f596de72118d46025060627c366
ri-yarm <thogusus@gmail.com>
2022-11-27 17:41:56 +0500

    do favicon

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-let
+const Popup = document.querySelector('.popup');
+const Profile = document.querySelector('.profile');
+
+const ClosePopupButton = Popup.querySelector('.popup__exit-button')
+const ProfileButton = Profile.querySelector('.profile__edit-profile')
+
+ProfileButton.addEventListener('click', function() {
+  Popup.classList.add('popup_opened')
+})
+
+ClosePopupButton.addEventListener('click', function() {
+  Popup.classList.remove('popup_opened')
+})
