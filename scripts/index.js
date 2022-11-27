361b61f1c41aff5d9283cb42d905555fcb78c5e0
ri-yarm <thogusus@gmail.com>
2022-11-27 19:00:01 +0500

    popup work

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,17 @@
 const Popup = document.querySelector('.popup');
 const Profile = document.querySelector('.profile');
+const Form = Popup.querySelector('.form');
 
-const ClosePopupButton = Popup.querySelector('.popup__exit-button')
-const ProfileButton = Profile.querySelector('.profile__edit-profile')
+const ClosePopupButton = Popup.querySelector('.popup__exit-button');
+const ProfileButton = Profile.querySelector('.profile__edit-profile');
+const SubmitButton = Form.querySelector('.form__submit');
 
-ProfileButton.addEventListener('click', function() {
-  Popup.classList.add('popup_opened')
+ProfileButton.addEventListener('click', () => {
+  Popup.classList.add('popup_opened');
 })
 
-ClosePopupButton.addEventListener('click', function() {
-  Popup.classList.remove('popup_opened')
+ClosePopupButton.addEventListener('click', () => {
+  Popup.classList.remove('popup_opened');
 })
+
+
